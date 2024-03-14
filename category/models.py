c1511159fd3b2dc004d539b04944cd260a922757
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-03-14 20:33:42 +0600

    Created modelform for task app and category app

--- category/models.py
+++ category/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from task.models import TaskModel
 # Create your models here.
+class CategoryModel(models.Model):
+    category_name = models.CharField(max_length=100)
+    tasks = models.ManyToManyField(TaskModel)
+    
+    def __str__(self) -> str:
+        return f'Name : {self.category_name}'
--- task/models.py
+++ task/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class TaskModel(models.Model):
+    task_title = models.CharField(max_length=100)
+    task_description = models.TextField()
+    is_completed = models.BooleanField(default=False)
+    task_assign_date = models.DateField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return f'Title : {self.task_title}'
